d5391f06fee7e85c6890f0386a1ee2a4a9e1aa84
Nikita <nik_zenkin@mail.ru>
2023-03-27 19:02:57 +0700

    added empty strings and corrected tabs

--- folder_Task1_var4_ver2/Task1_var4_ver2.c
+++ folder_Task1_var4_ver2/Task1_var4_ver2.c
@@ -5,87 +5,94 @@
 
 #define MAX 15
 
-int main_func(char * main_str)
+int main()
 {
-
-    char str[MAX] = {0}, str_max[MAX] = {0};
+    char main_str[MAX] = {0}, str[MAX] = {0}, str_max[MAX] = {0};
     int i = 0, count = 0, start= 0, length = MAX, end = 1;
+
+    fgets(main_str, MAX, stdin);
+
     char * p = main_str;
     char symb = *p;
     int main_length = strlen(main_str);
 
     while(length > 1)
     {
-
         count = 0;
-
         while(p < main_str + main_length)
         {
 
-        if(*p == symb)
-        {
-            str[i] = *p;
-        }
-        else
-        {
-            i = 0;
-            if(strlen(str) == 1 && str[i] != 10)
+            if(*p == symb)
             {
-                memset(str, 0, MAX);
                 str[i] = *p;
             }
+
             else
             {
-                if(strlen(str) > strlen(str_max))
+                i = 0;
+
+                if(strlen(str) == 1 && str[i] != 10)
                 {
-                    strcpy(str_max, str);
-                    end = count;
+                    memset(str, 0, MAX);
+                    str[i] = *p;
                 }
-                memset(str, 0, MAX);
-                str[i] = *p;
+
+                else
+                {
+
+                    if(strlen(str) > strlen(str_max))
+                    {
+                        strcpy(str_max, str);
+                        end = count;
+                    }
+
+                    memset(str, 0, MAX);
+                    str[i] = *p;
+                }
+
             }
+
+            if(*p != 10)
+            {
+                symb = *p;
+            }
+
+            *p++;
+            i++;
+            count++;
         }
-        if(*p != 10)
+        count = end;
+        length = strlen(str_max);
+
+        if(length < 2)
         {
-            symb = *p;
+            continue;
         }
-        *p++;
-        i++;
-        count++;
+
+        start = end - length + 1;
+
+        char * istr;
+
+        istr = strstr(main_str + start - 1, str_max);
+
+        printf("%d,%d%c\n", start, length, *istr);
+
+        if(length > 1)
+        {
+            strncpy(main_str + start - 1, memset(str_max, 0, MAX), length);
+            p = main_str;
         }
-    count = end;
-    length = strlen(str_max);
-    if(length < 2)
-    {
-        continue;
-    }
-    start = end - length + 1;
-    char * istr;
-    istr = strstr(main_str + start - 1, str_max);
-    printf("%d,%d%c\n", start, length, *istr);
-    if(length > 1)
-    {
-        strncpy(main_str + start - 1, memset(str_max, 0, MAX), length);
-        p = main_str;
-    }
+
     }
 
     for(p = main_str; p < main_str + main_length; p++)
     {
+
         if(*p != 0)
         {
             printf("%c", *p);
         }
+
     }
-    return length;
-}
-
-int main()
-{
-    char main_str[MAX] = {0};
-
-    fgets(main_str, MAX, stdin);
-    main_func(main_str);
-
     return 0;
 }
